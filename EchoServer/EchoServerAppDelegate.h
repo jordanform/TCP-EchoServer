72077d3cf6e29b33537dc28dbb30fc3aa6ff616f
jordanform <af.chen1983@gmail.com>
2016-09-26 11:27:42 -0700

    Add files via upload
    Initial commit.

---
+++ EchoServer/EchoServerAppDelegate.h
@@ -0,0 +1,27 @@
+#import <Cocoa/Cocoa.h>
+#import "GCDAsyncSocket.h"
+
+@class GCDAsyncSocket;
+
+
+@interface EchoServerAppDelegate : NSObject <NSApplicationDelegate, GCDAsyncSocketDelegate>
+{
+	dispatch_queue_t socketQueue;
+	
+	GCDAsyncSocket *listenSocket;
+	NSMutableArray *connectedSockets;
+	
+	BOOL isRunning;
+	
+	IBOutlet id logView;
+	IBOutlet id portField;
+	IBOutlet id startStopButton;
+	
+	NSWindow *__unsafe_unretained window;
+}
+
+@property (unsafe_unretained) IBOutlet NSWindow *window;
+
+- (IBAction)startStop:(id)sender;
+
+@end
